839d7868ceeb238bd84f301118919709096741c7
knorr3 <72547405+knorr3@users.noreply.github.com>
2022-05-20 14:18:10 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "consol-sandbox-5"
 
     workspaces {
       name = "sentinel-example"
